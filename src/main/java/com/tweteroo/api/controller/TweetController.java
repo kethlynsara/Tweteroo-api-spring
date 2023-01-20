928585c351227c104c55d8ce58cb80c0ffefce17
Kethlyn <kethlynsaraa@gmail.com>
2023-01-19 23:22:10 -0300

    feat: get tweets by username

--- src/main/java/com/tweteroo/api/controller/TweetController.java
+++ src/main/java/com/tweteroo/api/controller/TweetController.java
@@ -1,6 +1,10 @@
 package com.tweteroo.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +22,11 @@ public class TweetController {
 
     @Autowired
     private TweetRepository repository;
+
+    @GetMapping("/{USERNAME}") 
+    private List<Tweet> getByUsername(@PathVariable String USERNAME) {
+        return repository.findByUsername(USERNAME);
+    }
     
     @PostMapping
     public void create(@RequestBody @Valid TweetDTO req) {
