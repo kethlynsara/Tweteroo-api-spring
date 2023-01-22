cb2f75644fbe286eddc359574efd1e4f84e53a97
Kethlyn <kethlynsaraa@gmail.com>
2023-01-22 13:17:45 -0300

    feat: add pagination

--- src/main/java/com/tweteroo/api/controller/TweetController.java
+++ src/main/java/com/tweteroo/api/controller/TweetController.java
@@ -3,6 +3,10 @@ package com.tweteroo.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +20,7 @@ import com.tweteroo.api.service.TweetService;
 
 import jakarta.validation.Valid;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/tweets")
 public class TweetController {
@@ -26,6 +31,11 @@ public class TweetController {
     @GetMapping("/{USERNAME}") 
     private List<Tweet> getByUsername(@PathVariable String username) {
         return service.findByUsername(username);
+    }
+
+    @GetMapping
+    private Page<Tweet> getTweets(@PageableDefault(page = 0, size = 5) Pageable page) {
+        return service.findALl(page);
     }
     
     @PostMapping
--- src/main/java/com/tweteroo/api/service/TweetService.java
+++ src/main/java/com/tweteroo/api/service/TweetService.java
@@ -3,6 +3,10 @@ package com.tweteroo.api.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.tweteroo.api.dto.TweetDTO;
@@ -17,6 +21,16 @@ public class TweetService {
 
     public List<Tweet> findByUsername(String username) {
         return repository.findByUsername(username);
+    }
+
+    public Page<Tweet> findALl(Pageable pageable) {
+        int page = Integer.parseInt(pageable.getPageParameter());
+        int size = 5;
+        PageRequest pageRequest = PageRequest.of(page, size);
+        
+        return new PageImpl<>(
+            repository.findAll(),
+            pageRequest, size);
     }
 
     public void save(TweetDTO dto) {
